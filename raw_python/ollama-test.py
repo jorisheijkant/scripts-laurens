82c2167a1bf7e5ef99eb2b440fda0f5626b95c96
jorisheijkant <jorisheijkant@persgroep.net>
2025-01-17 13:22:08 +0100

    Ollama fixes (new version with typecasting)

--- raw_python/ollama-test.py
+++ raw_python/ollama-test.py
@@ -1,12 +1,13 @@
-import ollama 
+from ollama import chat
+from ollama import ChatResponse
 
 prompt = """
 Je krijgt een tweet te zien, laat weten of er in deze tweet sprake is van haat of niet.
-De tweet is in het Nederlands. Je kijkt specifiek naar misoygne termen en homohaat. 
+De tweet is in het Nederlands. Je kijkt specifiek naar misogyne termen (vrouwenhaat) en homohaat. 
 Geef een Boolean terug, dus False of True. Geef alleen de boolean terug.
 """
 
-test_tweet = "Ik hoop dat die vadsige teringhond eens van tv verdwijnt."
+test_tweet = "Teringwijf"
 
 messages = [
     {
@@ -19,10 +20,10 @@ messages = [
     }
 ]
 
-response = ollama.chat(model='llama3.2', messages=messages)
+response: ChatResponse = chat(model='llama3.1', messages=messages)
 
 try:
     ollama_response = response['message']['content']
-    print(ollama_response)
+    print(f"Does tweet '{test_tweet}' have hate? {ollama_response}")
 except Exception as e:
     print(f"Failing to parse Ollama answer: {e}")
